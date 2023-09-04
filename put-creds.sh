61bbcb134fb5834b00cc7d08bdbb91ea08d0707a
Taylor Thomas <taylor@cosmonic.com>
2023-09-04 13:44:03 -0700

    Tries to find NATS path first
    Signed-off-by: Taylor Thomas <taylor@cosmonic.com>

--- put-creds.sh
+++ put-creds.sh
@@ -3,4 +3,5 @@
 const_id=$(cosmo whoami -o json | jq -r '.user.constellation_ids[0]')
 password=$(jq -r '.token' ~/.cosmo/registry.token)
 payload=$(jq -n --arg const_id "$const_id" --arg password "$password" '{"registry.cosmonic.sh": {"registryType": "oci", "username": $const_id, "password": $password}}')
-~/go/bin/nats req -s 127.0.0.1:4223 "wasmbus.ctl.$const_id.registries.put" "$payload"
+nats=$(which nats || echo -n "~/go/bin/nats")
+$nats req -s 127.0.0.1:4223 "wasmbus.ctl.$const_id.registries.put" "$payload"
